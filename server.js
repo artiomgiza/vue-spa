ac927edbf8512d8b9c0ebf8bda131d7c5ae901a0
Artiom Giza <artiomg@gettaxi.com>
2018-05-31 15:01:37 +0300

    афтер ССР (модуле 7)

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path")
+const serialize = require("serialize-javascript")
+const { createBundleRenderer } = require('vue-server-renderer')
+let renderer
 
 const indexHTML = (() => {
   // path always be relative to server.js file with this:
@@ -10,11 +13,22 @@ const indexHTML = (() => {
 
 app.use('/dist', express.static(path.resolve(__dirname,'./dist')))
 
-require("./build/dev-server")(app);
+require("./build/dev-server")(app, bundle => {
+  renderer = createBundleRenderer(bundle)
+});
 
 app.get('*', (req, res) => {
-  res.write(indexHTML)
-  res.end();
+  const context = { url: req.url }
+  renderer.renderToString(context, (err, html) => {
+    if (err) {
+      return res.status(500).send('Server Error'+ err) 
+    }
+    html = indexHTML.replace('{{ APP }}', html)
+    html = html.replace('{{ STATE }}',
+    `<script>window.__INITIAL_STATE__=${serialize(context.initialState, { isJSON: true })}</script>`)
+    res.write(html)
+    res.end();
+  })
 })
 
 const port = process.env.PORT || 3000;
